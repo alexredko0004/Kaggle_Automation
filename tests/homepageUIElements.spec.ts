0c0a144ae9b0d29d5fb38f2cdb0883721345102c
alexredko0004 <alexredko0004@gmail.com>
2024-08-07 23:10:24 +0300

    added test for images comparison on Home page

---
+++ tests/homepageUIElements.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`/`);
+});
+
+test('Verify that images on home page correspond to screenshots', async ({ page }) => {
+
+  await test.step('Verify that Home page blocks images correspond to screenshots',async()=>{
+    const learnToCompeteBlockImage = page.locator(`div[style='background-image: url("/static/images/community/homepage/onboarding/track-card-bg1.svg");']`);
+    const takeAShortCourseBlockImage = page.locator(`div[style='background-image: url("/static/images/community/homepage/onboarding/track-card-bg2.svg");']`);
+    const browseInspiringDataBlockImage = page.locator(`div[style='background-image: url("/static/images/community/homepage/onboarding/track-card-bg3.svg");']`);
+    expect(await learnToCompeteBlockImage.screenshot()).toMatchSnapshot();
+    expect(await takeAShortCourseBlockImage.screenshot()).toMatchSnapshot();
+    expect(await browseInspiringDataBlockImage.screenshot()).toMatchSnapshot()
+  })
+  await test.step('Verify that main menu icons correspond to screenshots',async()=>{
+    const homeItemIcon = page.locator("i[title='Home']");
+    const competitionsIcon = page.locator("i[title='Competitions']");
+    const datasetsIcon = page.locator("i[title='Datasets']");
+    const modelsIcon = page.locator("i[title='Models']");
+    const codeIcon = page.locator("i[title='Code']");
+    const discussionsIcon = page.locator("i[title='Discussions']");
+    const learnIcon = page.locator("i[title='Learn']");
+    const yourWorkIcon = page.locator("i[title='Your Work']")
+    const viewActiveEventsIcon = page.getByLabel('Open Active Events dialog').nth(1);
+    expect(await homeItemIcon.screenshot()).toMatchSnapshot();
+    expect(await competitionsIcon.screenshot()).toMatchSnapshot();
+    expect(await datasetsIcon.screenshot()).toMatchSnapshot()
+    expect(await modelsIcon.screenshot()).toMatchSnapshot();
+    expect(await codeIcon.screenshot()).toMatchSnapshot();
+    expect(await discussionsIcon.screenshot()).toMatchSnapshot()
+    expect(await learnIcon.screenshot()).toMatchSnapshot();
+    expect(await yourWorkIcon.screenshot()).toMatchSnapshot();
+    expect(await viewActiveEventsIcon.screenshot()).toMatchSnapshot()
+  })
+  
+});
