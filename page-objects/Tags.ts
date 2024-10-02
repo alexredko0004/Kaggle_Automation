d266d1627c48479f4d53654b80462de1627da1a0
alexredko0004 <alexredko0004@gmail.com>
2024-10-02 23:13:46 +0300

    added tags selection via category to model tags test

--- page-objects/Tags.ts
+++ page-objects/Tags.ts
@@ -3,20 +3,24 @@ import { BasePage } from './BasePage'
 
 export class Tags extends BasePage{
     applyBtn: Locator
+    categoryItem: Locator
     closeBtn: Locator
     foundItem: Locator
     searchField: Locator
     selectedTagOnPanel: Locator
     tagsPanel: Locator
+    tagItem: Locator
 
     constructor(page){
         super(page)
         this.applyBtn = page.locator('.drawer-outer-container button').filter({hasText:'Apply'})
+        this.categoryItem = page.locator('.drawer-outer-container .category-select-menu_menu-items-wrapper [role="menuitem"]')
         this.closeBtn = page.locator('.drawer-outer-container').getByLabel('close')
         this.foundItem = page.locator('.drawer-outer-container [role="menuitem"]')
         this.searchField = page.locator('.drawer-outer-container input')
         this.selectedTagOnPanel = page.locator('button.selected')
         this.tagsPanel = page.locator('.drawer-outer-container')
+        this.tagItem = page.locator('.drawer-outer-container [role="menuitem"]')
     }
     /**
      * 
@@ -45,9 +49,37 @@ export class Tags extends BasePage{
             await this.page.waitForTimeout(1000)
         }
     }
+/**
+ * 
+ * @param categoryTag - string that follows pattern 'Category > Tag' (e.g. 'Geography And Places > Europe')
+ */
+    public async selectTagsFromCategory(categoryTag:string[]){
+        const categoryRegex = /(?<category>(\w+\s?)+)\s\>.*/
+        const tagRegex = /.*\>\s(?<tag>.*)/
+        for(let e of categoryTag){
+            const categoryArr = e.match(categoryRegex)
+            if (categoryArr!==null&&categoryArr.groups!==undefined){
+                await this.categoryItem.filter({hasText:categoryArr.groups.category}).click()
+            }
+            const tagArr = e.match(tagRegex)
+            if (tagArr!==null&&tagArr.groups!==undefined){
+                await this.tagItem.filter({hasText:tagArr.groups.tag}).click()
+            }
+        }
+        await this.page.waitForTimeout(1000)
+    }
 
     public async getArrayOfSelectedTags(){
+        await this.tagsPanel.waitFor();
         const selectedTags = await this.selectedTagOnPanel.locator('span:not([title="Close"])').allInnerTexts();
         return selectedTags
+    }
+
+    public async removeAllTags(){
+        await this.tagsPanel.waitFor();
+        const selectedTags = await this.getArrayOfSelectedTags();
+        for (let e of selectedTags){
+            await this.selectedTagOnPanel.getByTitle('Close').first().click()
+        }
     }
 }
