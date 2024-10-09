013a05b0b79ea51e5869cb10d26e2d69224de413
alexredko0004 <alexredko0004@gmail.com>
2024-10-09 15:02:46 +0300

    fixed UI comparison tests

--- tests/homepageUIElements.spec.ts
+++ tests/homepageUIElements.spec.ts
@@ -16,15 +16,15 @@ test('Verify that images on home page correspond to screenshots', async ({ page 
   })
   await test.step('Verify that main menu icons correspond to screenshots',async()=>{
     await page.getByText('OK, Got it.').click();
-    const homeItemIcon = page.locator("i[title='Home']");
-    const competitionsIcon = page.locator("i[title='Competitions']");
-    const datasetsIcon = page.locator("i[title='Datasets']");
-    const modelsIcon = page.locator("i[title='Models']");
-    const codeIcon = page.locator("i[title='Code']");
-    const discussionsIcon = page.locator("i[title='Discussions']");
-    const learnIcon = page.locator("i[title='Learn']");
-    const yourWorkIcon = page.locator("i[title='Your Work']")
-    const viewActiveEventsIcon = page.getByLabel('Open Active Events dialog').nth(1);
+    const homeItemIcon = page.locator("span[title='Home']");
+    const competitionsIcon = page.locator("span[title='Competitions']");
+    const datasetsIcon = page.locator("span[title='Datasets']");
+    const modelsIcon = page.locator("span[title='Models']");
+    const codeIcon = page.locator("span[title='Code']");
+    const discussionsIcon = page.locator("span[title='Discussions']");
+    const learnIcon = page.locator("span[title='Learn']");
+    const yourWorkIcon = page.locator("span[title='Your Work']")
+    const viewActiveEventsIcon = page.getByLabel('Open Active Events dialog');
     expect(await homeItemIcon.screenshot()).toMatchSnapshot();
     expect(await competitionsIcon.screenshot()).toMatchSnapshot();
     expect(await datasetsIcon.screenshot()).toMatchSnapshot()
